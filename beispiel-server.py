c24c49204756f9531f91b3c1147387baa496c4af
TollerName1337 <nichtvergeben123@gmail.com>
2024-05-22 10:42:31 +0200

    1. Added endpoints: - patch lists - post todos - patch, delete todos
    2. Changed uuid's from todos to string
    3. Allowed PATCH as methode

--- beispiel-server.py
+++ beispiel-server.py
@@ -19,11 +19,11 @@ app = Flask(__name__)
 todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
 todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
 todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
-todo_1_id = uuid.uuid4()
-todo_2_id = uuid.uuid4()
-todo_3_id = uuid.uuid4()
-todo_4_id = uuid.uuid4()
-
+todo_1_id = str(uuid.uuid4())
+todo_2_id = str(uuid.uuid4())
+todo_3_id = str(uuid.uuid4())
+todo_4_id = str(uuid.uuid4())
+print(todo_1_id)
 # define internal data structures with example data
 todo_lists = [
     {'id': todo_list_1_id, 'name': 'Einkaufsliste'},
@@ -34,19 +34,19 @@ todos = [
     {'id': todo_1_id, 'name': 'Milch', 'description': '', 'list': todo_list_1_id},
     {'id': todo_2_id, 'name': 'Arbeitsblätter ausdrucken', 'description': '', 'list': todo_list_2_id},
     {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id},
-    {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id},
+    {'id': todo_4_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id},
 ]
 
 # add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
 @app.after_request
 def apply_cors_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE,PATCH'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
 # define endpoint for getting and deleting existing todo lists
-@app.route('/list/<list_id>', methods=['GET', 'DELETE'])
+@app.route('/list/<list_id>', methods=['GET', 'DELETE', 'PATCH'])
 def handle_list(list_id):
     # find todo list depending on given list id
     list_item = None
@@ -66,6 +66,9 @@ def handle_list(list_id):
         print('Deleting todo list...')
         todo_lists.remove(list_item)
         return '', 200
+    elif request.method == 'PATCH':
+        list_item['name'] = request.json['name']
+        return '',200
 
 
 # define endpoint for adding a new list
@@ -85,6 +88,40 @@ def add_new_list():
 def get_all_lists():
     return jsonify(todo_lists)
 
+# define endpoint for patching and deleting items in a list
+@app.route('/list/<list_id>/item/<item_id>', methods=['PATCH', 'DELETE'])
+def handle_list_item(list_id, item_id):
+    item = None
+    for todo in todos:
+        if(todo['id'] == item_id and todo['list'] == list_id):
+            item = todo
+            
+    if request.method == 'PATCH':
+        todo['name'] = request.json['name']
+        todo['description'] = request.json['beschreibung']
+        return 'item patched',200
+    elif request.method == 'DELETE':
+        todos.remove(todo);
+        return 'item deleted',200
+    return 'list or item not found', 500
+
+#define endpoint for adding an item into a list
+@app.route('/list/<list_id>/item', methods=['POST'])
+def add_item_to_list(list_id):
+    list = None
+    for l in todo_lists:
+        if(l['id'] == list_id):
+            newItem = request.json
+            newItem['list'] = l['id']
+            newItem['id'] = str(uuid.uuid4)
+
+    if(list == None):
+        return 'list not found', 404
+
+    if(newItem == None):
+        return 'item couldnt be created',500
+
+    return 'item created',201
 
 if __name__ == '__main__':
     # start Flask server
